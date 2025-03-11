66fa7ad4fa7b9b148f57a87b1eaed19d8a21ffc9
Rober <robercantalejo@gmail.com>
2025-03-11 21:01:00 +0000

    Hago las tablas y las relaciono

--- src/models.py
+++ src/models.py
@@ -1,33 +1,65 @@
 import os
 import sys
 from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
-from sqlalchemy import create_engine, String, ForeignKey
+from sqlalchemy import create_engine, String, ForeignKey, LargeBinary
 from eralchemy2 import render_er
+from passlib.hash import bcrypt_sha256
+from typing import Optional, List
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False) # Aquí ponemos que el username debe ser único para no repetir y que el límite de carácteres es 50.
+    password: Mapped[str] = mapped_column(String(200), nullable=False)
+    profile_image: Mapped[Optional[bytes]] = mapped_column(LargeBinary, nullable=True) # Almacenamos las imágines de perfil en bytes, haciéndolo opcional y pudiendo dejarse en blanco.
+
+    def encrypt_pass(self, raw_pass: str) -> None: # Para encriptar la contraseña usamos bcrypt_sha256. Pasamos una contraseña y la encriptamos.
+        self.password = bcrypt_sha256.hash(raw_pass)
+
+    def verify_pass(self, raw_pass: str) -> bool: # Validamos la contraseña encriptada. Como la comparamos usando bcrypt_sha256 debería dar True.
+        return bcrypt_sha256.verify(raw_pass, self.password)
+
+class People(Base):
+    __tablename__ = 'people'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
-    address: Mapped["Address"] = relationship(back_populates="person")
+    height: Mapped[str] = mapped_column(nullable=False)
+    mass: Mapped[str] = mapped_column(nullable=False)
+    birth_year: Mapped[str] = mapped_column(nullable=False)
+    favorites: Mapped[List["Favorites"]] = relationship(back_populates="people") # Relación hacia favoritos.
 
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planets(Base):
+    __tablename__ = 'planets'
     id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
-    person_id: Mapped[int] = mapped_column(ForeignKey("person.id"))
-    person: Mapped["Person"] = relationship(back_populates="address")
+    climate: Mapped[str] = mapped_column(nullable=False)
+    terrain: Mapped[str] = mapped_column(nullable=False)
+    population: Mapped[str] = mapped_column(nullable=False)
+    favorites: Mapped[List["Favorites"]] = relationship(back_populates="planet") # Relación hacia favoritos.
 
-    def to_dict(self):
-        return {}
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    model: Mapped[str] = mapped_column(nullable=False)
+    manufacturer: Mapped[str] = mapped_column(nullable=False)
+    cost: Mapped[str] = mapped_column(nullable=False)
+    favorites: Mapped[List["Favorites"]] = relationship(back_populates="vehicles") # Relación hacia favoritos.
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    # Estos datos de la tabla vienen de datos de otras tablas, por lo que los definimos.
+    people_id: Mapped[int] = mapped_column(ForeignKey("people.id"))
+    planets_id: Mapped[int] = mapped_column(ForeignKey("planets.id"))
+    vehicles_id: Mapped[int] = mapped_column(ForeignKey("vehicles.id"))
+
+    # Relaciones de vuelta a cada una de las tablas anteriores
+    user: Mapped["User"] = relationship(back_populates="favorites")
+    people: Mapped["People"] = relationship(back_populates="favorites")
+    planet: Mapped["Planets"] = relationship(back_populates="favorites")
+    vehicle: Mapped["Vehicles"] = relationship(back_populates="favorites")
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
